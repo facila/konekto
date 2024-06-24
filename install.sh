3cdf267d96c4d59a961f3c4952f7aea60d11d076
facila <facila@gmx.fr>
2024-06-24 11:18:57 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# version 1.00 janvier 2024
+# sh install.sh FICHIER
+
+proc_exit ()
+{
+perl -e "$1" 2>/dev/null
+[ $? != "0" ] && { echo "  vous devez d'abbord installer : $2" ; exit ; }
+}
+
+#################################################################################
+
+ FILE=$1
+APPLI=`echo $FILE | cut -f1 -d.`
+  EXT=`echo $FILE | cut -f4-5 -d.`
+  DIR=$PWD/$APPLI-main  
+
+FILE=$DIR/$FILE
+[ "$EXT" != "tar.gz" ] && { echo "le fichier $FILE doit être un tar.gz" ; exit ; }
+[ ! -s "$FILE"       ] && { echo "fichier $FILE absent" ; exit ; }
+
+echo "vérification des dépendances"
+proc_exit '' perl
+proc_exit 'use Expect' Expect.pm
+proc_exit 'use IO::Pty'
+proc_exit 'use IO::Tty' 
+
+echo "verification de facila"
+if [ "$FACILA" = "" ]
+then FACILA=$PWD/facila
+     echo "  ajout de FACILA dans .bashrc"
+     printf "\n# FACILA\nexport FACILA=$FACILA\n" >> ~/.bashrc
+fi
+
+echo "installation de $FILE"
+cd $FACILA
+tar -pxzf $FILE
